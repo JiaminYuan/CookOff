ab72fcc89401e9ec0b169147d3228353b10de053
EaEmmy <82058275+EaEmmy@users.noreply.github.com>
2023-04-26 09:34:23 -0400

    Animations
    anim

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,13 +12,23 @@ public class PlayerMovement : MonoBehaviour
     private float gravityValue = -9.81f;
     private Vector2 turn;
 
+    public Animator ac;
+    private float walkSpeed = 5;
+    private float runSpeed = 8; 
+    private float charSpeed; 
+    private float movementSpeed;
+
+
     private void Start()
     {
         controller = gameObject.GetComponent<CharacterController>();
+        ac = GetComponent<Animator>();
     }
 
     void Update()
     {
+        UpdateAnimator();
+
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
@@ -30,6 +40,7 @@ public class PlayerMovement : MonoBehaviour
         turn.x = Input.GetAxis("Mouse X");
         turn.y = Input.GetAxis("Mouse Y");
         Vector3 movement = ((v *transform.forward *playerSpeed + h * transform.right * playerSpeed)); 
+        movementSpeed = movement.magnitude;
         //Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         controller.Move(movement * Time.deltaTime * playerSpeed);
         transform.Rotate(0, turn.x, 0, Space.Self);
@@ -39,6 +50,37 @@ public class PlayerMovement : MonoBehaviour
        
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    void UpdateAnimator()
+    {
+        float speed = GetMovementSpeed();
+        groundedPlayer = controller.isGrounded;
+
+        if (movementSpeed == 0)
+        {
+            ac.SetFloat("Character Speed", 0);
+        }
+        else if (speed == walkSpeed)
+        {
+            ac.SetFloat("Character Speed", 0.5f);
+        }
+        else if (speed == runSpeed)
+        {
+            ac.SetFloat("Character Speed", 1);
+        }
+    }
+
+    float GetMovementSpeed()
+    {
+        if (Input.GetButton("Fire3")) // Left shift
+        {
+            return runSpeed;
+        }
+        else
+        {
+            return walkSpeed;
+        }
     }
 
 }
