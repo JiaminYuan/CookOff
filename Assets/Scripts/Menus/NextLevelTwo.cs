8cfe411f5fce99f1b9923a4efeceb6aeb8ffc2b8
EaEmmy <82058275+EaEmmy@users.noreply.github.com>
2023-05-05 09:09:34 -0400

    transition level scenes + timer

---
+++ Assets/Scripts/Menus/NextLevelTwo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NextLevelTwo : MonoBehaviour
+{
+    public Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick(){
+        SceneManager.LoadScene("Main");
+    }
+}
